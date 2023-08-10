f78c30b65073b5412ca06c03b5942c45c9af4efc
Anton Ermolaev <ant.ermolaev@gmail.com>
2023-08-10 12:12:20 +0300

    Put assertions for name, email and password into separate tests

--- tests/test_registration_page.py
+++ tests/test_registration_page.py
@@ -6,7 +6,7 @@ import helpers
 
 
 class TestRegistration:
-    def test_registration_true(self, driver):
+    def test_registration_name_not_empty(self, driver):
         WebDriverWait(driver, 3).until(EC.element_to_be_clickable(PageLocators.LOGIN_INTO_ACCOUNT_BUTTON))
         driver.find_element(*PageLocators.LOGIN_INTO_ACCOUNT_BUTTON).click()
 
@@ -18,14 +18,42 @@ class TestRegistration:
         name_input = driver.find_element(*PageLocators.NAME_INPUT).get_attribute('value')
         assert name_input != '', 'Fill in the name'
 
+    def test_registration_email_is_valid(self, driver):
+        WebDriverWait(driver, 3).until(EC.element_to_be_clickable(PageLocators.LOGIN_INTO_ACCOUNT_BUTTON))
+        driver.find_element(*PageLocators.LOGIN_INTO_ACCOUNT_BUTTON).click()
+
+        WebDriverWait(driver, 3).until(EC.visibility_of_element_located(PageLocators.LOGIN_HEADER))
+        driver.find_element(*PageLocators.REGISTER_BUTTON_ON_LOGIN).click()
+
         driver.find_element(*PageLocators.EMAIL_INPUT).send_keys(helpers.random_email())
         email_input = driver.find_element(*PageLocators.EMAIL_INPUT).get_attribute('value')
         assert helpers.is_valid_email(email_input), 'Invalid email format'
+
+    def test_registration_password_is_six_or_more_symbols(self, driver):
+        WebDriverWait(driver, 3).until(EC.element_to_be_clickable(PageLocators.LOGIN_INTO_ACCOUNT_BUTTON))
+        driver.find_element(*PageLocators.LOGIN_INTO_ACCOUNT_BUTTON).click()
+
+        WebDriverWait(driver, 3).until(EC.visibility_of_element_located(PageLocators.LOGIN_HEADER))
+        driver.find_element(*PageLocators.REGISTER_BUTTON_ON_LOGIN).click()
 
         driver.find_element(*PageLocators.PASSWORD_INPUT).send_keys(helpers.random_password())
         password_input = driver.find_element(*PageLocators.PASSWORD_INPUT).get_attribute('value')
         assert len(password_input) >= 6, 'Password is too short'
 
+    def test_registration_true(self, driver):
+        WebDriverWait(driver, 3).until(EC.element_to_be_clickable(PageLocators.LOGIN_INTO_ACCOUNT_BUTTON))
+        driver.find_element(*PageLocators.LOGIN_INTO_ACCOUNT_BUTTON).click()
+
+        WebDriverWait(driver, 3).until(EC.visibility_of_element_located(PageLocators.LOGIN_HEADER))
+        driver.find_element(*PageLocators.REGISTER_BUTTON_ON_LOGIN).click()
+
+        WebDriverWait(driver, 3).until(EC.element_to_be_clickable(PageLocators.NAME_PLACEHOLDER))
+        driver.find_element(*PageLocators.NAME_INPUT).send_keys(data.name)
+        driver.find_element(*PageLocators.EMAIL_INPUT).send_keys(helpers.random_email())
+        email_input = driver.find_element(*PageLocators.EMAIL_INPUT).get_attribute('value')
+
+        driver.find_element(*PageLocators.PASSWORD_INPUT).send_keys(helpers.random_password())
+        password_input = driver.find_element(*PageLocators.PASSWORD_INPUT).get_attribute('value')
         driver.find_element(*PageLocators.REGISTRATION_BUTTON_ON_REGISTER).click()
 
         WebDriverWait(driver, 3).until(EC.visibility_of_element_located(PageLocators.LOGIN_HEADER))
